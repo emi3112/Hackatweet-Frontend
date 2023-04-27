11d2105587379995668b22e72855611e393ea661
abde <infoabde@gmail.com>
2023-04-27 18:22:41 +0200

    resolution des conflits

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,6 @@
 import '../styles/globals.css';
 import Head from 'next/head';
 
-
-
-// redux imports
-import { Provider } from 'react-redux';
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import likes from '../reducers/liketweets'; //?????
-
-
-
-import { persistStore, persistReducer } from 'redux-persist';
-import { PersistGate } from 'redux-persist/integration/react';
-
-import storage from 'redux-persist/lib/storage';
-
-const reducers = combineReducers({likes,}); // listerles reducers ----
-const persistConfig = { key: 'tweetsstore', storage };
-
-const store = configureStore({
-  reducer: persistReducer(persistConfig, reducers),
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
-});// ---persistor
-
-
-
-const persistor = persistStore(store); // ---persistor
 import { Provider } from 'react-redux';
 import user from '../reducers/user';
 import { persistStore, persistReducer } from 'redux-persist';
@@ -33,7 +8,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import storage from 'redux-persist/lib/storage';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
-const reducers = combineReducers({user});
+const reducers = combineReducers({likes,user});
 const persistConfig = { key: 'applicationName', storage };
 
 
@@ -48,8 +23,7 @@ const store = configureStore({
 
  function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-       <PersistGate persistor={persistor}>
+ 
      <Provider store={store}>
       <PersistGate persistor={persistor}>
       <Head>
@@ -58,7 +32,7 @@ const store = configureStore({
       <Component {...pageProps} />
       </PersistGate>
       </Provider>
-    </>
+  
   );
 }
 
